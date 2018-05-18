b9e080d0f36a3aa935516f671174a51844d0cacb
Chris Stefano <virtualstaticvoid@gmail.com>
2018-05-18 20:23:09 +0100

    only run subset of tests

--- package-tests/app.R
+++ package-tests/app.R
@@ -1,2 +1,4 @@
 library(tools)
-testInstalledPackages()
+
+# https://www.rdocumentation.org/packages/tools/versions/3.4.3/topics/testInstalledPackage
+testInstalledPackages(scope=c("recommended"), types=c("vignettes"))
